1353d1807356798a61aa69ea535e5a2602d09759
Matthew Phillips <phillips.matthew829@gmail.com>
2023-09-22 13:26:00 -0500

    commit tree 1d46fd9215bbd0dd2a1aea94fcfe1eaf5945c297

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
     has_many :recipes
 
     validates :username, presence: true, uniqueness: true
+
+    validates :password, presence: true, length: {minimum: 5}
 end
